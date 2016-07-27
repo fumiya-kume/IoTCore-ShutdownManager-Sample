257c38b0ec08981d6f4de7b2606a5b04a5354169
久米　史也 <fumiya.kume@hotmail.com>
2016-07-28 01:38:28 +0900

    Init

---
+++ Sample1/Sample1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace Sample1
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ShutdownButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ShutdownKind =Windows.System.ShutdownKind.Shutdown;
+            ShutdownManager.BeginShutdown(ShutdownKind,TimeSpan.Zero);
+        }
+
+        private void RebootButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ShutdownKind = Windows.System.ShutdownKind.Restart;
+            ShutdownManager.BeginShutdown(ShutdownKind, TimeSpan.FromMinutes(3));
+        }
+    }
+}
